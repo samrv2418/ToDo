0a3c549e445bca558fff473cfb61ed4020120ba9
samrv2418 <samrv2418@gmail.com>
2022-08-05 19:44:22 +0530

    fix Mobile view

--- src/components/UpdateTaskForm.jsx
+++ src/components/UpdateTaskForm.jsx
@@ -1,9 +1,14 @@
 const UpdateTaskForm=({updateData,changeHolder,updateTask,cancleUpdate})=>
 {
+    if(document.querySelector(".update-task"))
+    {
+        const element=document.querySelector(".update-task")
+        element.classList.add('col-12')
+    }
     return (
         <>
             <div className='row'>
-                <div className='col'>
+                <div className='col-12 col-sm update-input'>
                     <input
                         value={updateData && updateData.title}
                         onChange={changeHolder}
